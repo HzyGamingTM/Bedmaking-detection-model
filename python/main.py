894e265c80b3baa442ad2053fbecc0c836225499
hzy <zhengyonghoe@gmail.com>
2025-11-14 14:13:42 +0800

    asd

--- python/main.py
+++ python/main.py
@@ -5,29 +5,23 @@ import yolov5 as yv5
 import os
 import psutil
 
-
-def array_to_text(np_array):
-    # Flatten to 1D, convert to strings, join with commas
-    flat = np_array.flatten()
-    return ",".join(map(str, flat))
-
 class TrainingMgr:
-    def __init__(self, path_to_data):
-        if not os.path.exists(path_to_data):
-            print("File does not exists!")
-            return None
-        
-        self.path_to_data = path_to_data
-
-        self.image_data = []
-        self.crumpled_data = []
-        self.combined_data = []
+    def __init__(self):
+        self.counter = 0 
         pass
 
-    def collect_data(self, np_data_array, crumpled):
-        self.image_data.append(np_data_array) # Array of array of images
-        self.crumpled_data.append(crumpled) # Array of crumple status
-        self.combined_data.append([array_to_text(np_data_array), str(crumpled)])
+    def collect_data(self, image_data, crumpled):
+        crumpled_status = "error"
+        if crumpled:
+            crumpled_status = "crumpled"
+        else:
+            crumpled_status = "uncrumpled"
+
+        filename = f"{str(self.counter)}_{crumpled_status}.png"
+        output_path = os.path.join("data", filename)
+        cv2.imwrite(output_path, image_data)
+        print(f"Saved as {filename}")
+        self.counter += 1
         pass
 
     def get_memory_usage(self):
@@ -35,11 +29,6 @@ class TrainingMgr:
         mem = process.memory_info().rss  # in bytes
         print(f"Memory usage: {mem / 1024**2:.2f} MB")
         pass
-
-    def cleanup_and_save(self):
-        with open(self.path_to_data, "a") as file:
-            for data in self.combined_data:
-                file.write(f"{data[0]}|{data[1]}\n")
 
 
 class Resolution:
@@ -85,26 +74,25 @@ class Camera:
                     break
 
                 if key == self.screenshot_crumpled_key:
-                    print("Taken photo crumpled")
+                    print("Taken photo | Crumpled")
                     self.training_mgr.collect_data(color_image, True)
-                    pass
+ 
 
                 elif key == self.screenshot_uncrumpled_key:
-                    print("Taken photo uncrumpled")
+                    print("Taken photo | Uncrumpled")
                     self.training_mgr.collect_data(color_image, False)
-                    pass
 
                 elif key == 77:
+                    print("Getting Memory")
                     self.training_mgr.get_memory_usage()
-                    pass
-
+                    
         finally:
             self.pipeline.stop()
             cv2.destroyAllWindows()
         
 
 def main():
-    training_mgr = TrainingMgr("data.txt")
+    training_mgr = TrainingMgr()
     res = Resolution(1280, 720)
     camera_settings = CameraSettings(resolution=res, framerate=30)
     
@@ -112,8 +100,6 @@ def main():
     camera = Camera(camera_settings, screenshot_crumpled_key=49, screenshot_uncrumpled_key=50, training_mgr=training_mgr)
     
     camera.camera_process() # Starts camera process loop
-    
-    camera.training_mgr.cleanup_and_save()
     pass
 
 if __name__ == "__main__":
